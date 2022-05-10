dec518c0692bfbd05f3d09814e357511a395cf74
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-10 11:16:44 +0630

    Add config for fonts"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
+    fontFamily: {
+      oswald: ['Oswald', 'sans-serif'],
+      varela: ['Varela Round', 'sans-serif'],
+    },
   },
   plugins: [],
 };
