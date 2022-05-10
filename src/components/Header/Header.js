4cb4f0dcec2207c5a57943febbf42d6c8a361b10
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-10 12:11:46 +0630

    Add styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <header className='bg-blue-500 py-3 px-3 flex justify-center text-slate-50'>
-        <h1 className='text-2xl font-bold font-oswald'>CV Creator</h1>
+      <header className='bg-purple-600 py-3 px-3 flex justify-center text-slate-50'>
+        <h1 className='text-2xl font-bold font-oswald hover:text-purple-300'>
+          <a href='/'>CV Creator</a>
+        </h1>
       </header>
     );
   }
