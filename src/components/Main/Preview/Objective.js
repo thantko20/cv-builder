84d2b7dd4002bd9566df36fe824f027780d882cf
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-10 19:30:18 +0630

    Edit text colour

--- src/components/Main/Preview/Objective.js
+++ src/components/Main/Preview/Objective.js
@@ -5,7 +5,7 @@ class Objective extends Component {
     return (
       <div>
         <h3 className='text-lg font-semibold mb-2'>Objective</h3>
-        <p className='text-gray-700'>{this.props.objective}</p>
+        <p className='text-gray-700 text-sm'>{this.props.objective}</p>
       </div>
     );
   }
