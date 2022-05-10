ed90adfc61f3e9950dfd3ad1d3a0871e62485347
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-10 21:30:24 +0630

    Add mock for Experience.js

---
+++ src/components/Main/Preview/Experience.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import ExperienceRow from './ExperienceRow';
+
+class Experience extends Component {
+  render() {
+    const { experience } = this.props;
+
+    return (
+      <div>
+        <h3 className='text-lg font-semibold mb-2'>Experience</h3>
+        <div>
+          {experience.map((info) => (
+            <ExperienceRow
+              from={info.from}
+              to={info.to}
+              jobTitle={info.jobTitle}
+              company={info.company}
+              summary={info.summary}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Experience;
