33a4a76495027e6e27ef549804be3ad0fac47765
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-11 13:22:14 +0630

    Add skeleton general info

--- src/components/Main/EditMode/EditMode.js
+++ src/components/Main/EditMode/EditMode.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
+import GeneralInfo from './GeneralInfo';
 
 class EditMode extends Component {
   render() {
-    return <div className='bg-white w-[760px] flex-shrink-0'>Hello</div>;
+    return (
+      <div className='p-5 bg-white w-[760px] flex-shrink-0'>
+        <GeneralInfo />
+      </div>
+    );
   }
 }
 
