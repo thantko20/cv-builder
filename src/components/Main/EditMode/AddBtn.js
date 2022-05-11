2520e0e09ba7712851d88ecb408703fa936bed49
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-11 20:26:22 +0630

    Styling

--- src/components/Main/EditMode/AddBtn.js
+++ src/components/Main/EditMode/AddBtn.js
@@ -2,7 +2,14 @@ import React, { Component } from 'react';
 
 class AddBtn extends Component {
   render() {
-    return <button onClick={this.props.onClick}>Add</button>;
+    return (
+      <button
+        onClick={this.props.onClick}
+        className='bg-blue-300 px-5 py-1 text-sm font-semibold text-blue-900 rounded hover:bg-blue-400 mt-2'
+      >
+        Add
+      </button>
+    );
   }
 }
 
