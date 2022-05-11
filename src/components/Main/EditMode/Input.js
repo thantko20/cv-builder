b9c64a936c6c6ee6d48650c9a7ce09c1d5e8daba
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-11 13:21:22 +0630

    Styling

--- src/components/Main/EditMode/Input.js
+++ src/components/Main/EditMode/Input.js
@@ -5,7 +5,12 @@ class Input extends Component {
     const { placeHolder, value, onChange } = this.props;
 
     return (
-      <input placeholder={placeHolder} value={value} onChange={onChange} />
+      <input
+        placeholder={placeHolder}
+        value={value}
+        onChange={onChange}
+        className='bg-blue-50 px-1 py-1 text-sm font-medium appearance-none placeholder:text-gray-400 outline-none border rounded focus:border-gray-500'
+      />
     );
   }
 }
