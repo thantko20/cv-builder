71b2e4592eeeb11b12924cc2e1ccd4223bdeb83a
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-11 20:01:15 +0630

    Style

--- src/components/Main/EditMode/DelBtn.js
+++ src/components/Main/EditMode/DelBtn.js
@@ -5,8 +5,12 @@ import React, { Component } from 'react';
 class DelBtn extends Component {
   render() {
     return (
-      <button onClick={this.props.onClick}>
-        <FontAwesomeIcon icon={faTrashCan} />
+      <button
+        onClick={this.props.onClick}
+        className='text-red-700 bg-red-100 self-start transition-colors duration-200 py-1 px-2 text-sm font-semibold hover:bg-red-200 rounded'
+      >
+        <FontAwesomeIcon icon={faTrashCan} className='mr-1 text-xs' />
+        <span>Delete</span>
       </button>
     );
   }
