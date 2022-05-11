7e101c96d5bba66613e137b0fe3537a64e2c4bb1
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-11 12:55:30 +0630

    First commit

---
+++ src/components/Main/EditMode/EditMode.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class EditMode extends Component {
+  render() {
+    return <div className='bg-white w-[760px] flex-shrink-0'>Hello</div>;
+  }
+}
+
+export default EditMode;
