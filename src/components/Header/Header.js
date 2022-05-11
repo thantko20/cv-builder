c19d35f08cd258d502890cccb45121dee9134207
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-11 16:40:31 +0630

    Add fa icon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,12 @@
+import { faFile } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { Component } from 'react';
 
 class Header extends Component {
   render() {
     return (
-      <header className='bg-purple-600 py-3 px-3 flex justify-center text-slate-50'>
+      <header className='bg-purple-600 py-3 px-3 flex justify-center text-slate-50 items-center gap-2'>
+        <FontAwesomeIcon icon={faFile} className='text-lg' />
         <h1 className='text-2xl font-bold font-oswald hover:text-purple-300'>
           <a href='/'>CV Creator</a>
         </h1>
