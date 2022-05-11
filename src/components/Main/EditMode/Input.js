2cf9ace066f53781e28fee8940e85c0136eb137a
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-11 15:55:43 +0630

    Add type for input el

--- src/components/Main/EditMode/Input.js
+++ src/components/Main/EditMode/Input.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 
 class Input extends Component {
   render() {
-    const { placeHolder, value, onChange } = this.props;
+    const { placeHolder, type, value, onChange } = this.props;
 
     return (
       <input
         placeholder={placeHolder}
+        type={type}
         value={value}
         onChange={onChange}
         className='bg-blue-50 px-1 py-1 text-sm font-medium appearance-none placeholder:text-gray-400 outline-none border rounded focus:border-gray-500'
