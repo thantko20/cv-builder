385708de375f4ca66f72d932a61ac93e3de88b5d
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-12 10:49:30 +0630

    Add jit mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  mode: 'jit',
+  content: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
     fontFamily: {
